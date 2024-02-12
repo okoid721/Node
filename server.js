d4f56dc567b9becb3747691cfdd2ba9a9ac0ff13
okoid721 <okoid721@gmail.com>
2024-02-12 21:21:15 +0100

    node

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/contacts');
+app.get('/api/contacts', require('./routes/contactRoutes'));
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
