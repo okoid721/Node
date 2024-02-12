932bbfcc0f059f30aa80364471c8b6145116ab2c
okoid721 <okoid721@gmail.com>
2024-02-13 00:04:42 +0100

    node

--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,6 +1,43 @@
+const { constant } = require('../constant');
+
 const errorHandle = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.json({ message: err.message, stackTrace: err.stack });
+  switch (statusCode) {
+    case constant.VALIDATION_ERROR:
+      res.json({
+        title: 'Validation Failed',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+      break;
+    case constant.NOT_FOUND:
+      res.json({
+        title: 'Not Found',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constant.UNAUTHORIZED:
+      res.json({
+        title: 'Un authorized',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constant.FORBIDDEN:
+      res.json({
+        title: 'Forbidden',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constant.SERVER_ERROR:
+      res.json({
+        title: 'Server error',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    default:
+      break;
+  }
 };
 
 module.exports = errorHandle;
