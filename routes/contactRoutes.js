73c6f659a6a3b0ccb0f81c4fb41ff7b196072e6b
okoid721 <okoid721@gmail.com>
2024-02-12 21:33:31 +0100

    node

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/', (req, res) => {
+router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Get all contacts' });
 });
+router.route('/').post((req, res) => {
+  res.status(200).json({ message: 'Create contact' });
+});
+router.route('/:id').get((req, res) => {
+  res.status(200).json({ message: `Get contact ${req.params.id}` });
+});
+router.route('/:id').put((req, res) => {
+  res.status(200).json({ message: `update contact ${req.params.id}` });
+});
+router.route('/:id').get((req, res) => {
+  res.status(200).json({ message: `delete contact ${req.params.id}` });
+});
 
-module.export = router;
+module.exports = router;
