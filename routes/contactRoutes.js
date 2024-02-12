73df035b0042d4113ed13117128c678b51156dfb
okoid721 <okoid721@gmail.com>
2024-02-12 14:44:50 +0100

    node

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/api/contacts', (req, res) => {
+router.route('/', (req, res) => {
   res.status(200).json({ message: 'Get all contacts' });
 });
